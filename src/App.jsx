8aa68223accce5315db354868abc00692d24385a
hojncode <glicm12@gmail.com>
2022-12-06 07:03:48 +0900

    Theme fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,23 @@
 import styled from "styled-components";
-import { keyframes } from "styled-components";
 
-const Wrap = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-const Text = styled.span`
+const Title = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
-const Input = styled.input.attrs({
-  required: true,
-  placeholder: "attrs",
-  minLength: 10,
-})`
-  background-color: tomato;
-`;
-
-export const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    border-radius:100px;
-  }
-
-  100%{
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  background-color: ${(props) => props.bgC};
-  height: 200px;
-  width: 200px;
+const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotationAnimation} 10s linear infinite;
-  ${Emoji}:hover {
-    font-size: 198px;
-  }
-  flex-direction: column;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
-    <>
-      <Wrap>
-        <Text as="a">as="a"</Text>
-        <Input />
-        <Input />
-        <Input />
-        <Box bgC="teal">
-          <Emoji>🍎</Emoji>
-          <Circle bgC="red"></Circle>
-        </Box>
-      </Wrap>
-    </>
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
 
-const darkMode = {
-  textColor:"white",
-  backgroundColor:"#111"
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
 };
 
-const lightMode = {
-  textColor:"#111",
-  backgroundColor:"white"
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
 };
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <ThemeProvider theme={darkMode}>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
