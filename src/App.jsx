524667f6b3a2ae09c9af554e6e4687eb70d70a99
hojncode <glicm12@gmail.com>
2022-12-06 06:58:13 +0900

    Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import styled from "styled-components";
-import {keyframes} from "styled-components"
+import { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrap = styled.div`
+  height: 100vh;
+  width: 100vw;
   display: flex;
-  flex-direction:column;
+  flex-direction: column;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 const Text = styled.span`
-  color: black;
+  color: ${(props) => props.theme.textColor};
 `;
 
-const Input = styled.input.attrs({ required: true ,placeholder: "attrs", minLength:10})`
+const Input = styled.input.attrs({
+  required: true,
+  placeholder: "attrs",
+  minLength: 10,
+})`
   background-color: tomato;
 `;
 
-const rotationAnimation = keyframes`
+export const rotationAnimation = keyframes`
   0% {
     transform:rotate(0deg);
     border-radius:0px;
@@ -26,42 +33,43 @@ const rotationAnimation = keyframes`
     transform:rotate(360deg);
     border-radius:0px;
   }
-`
-
+`;
 
 const Emoji = styled.span`
   font-size: 36px;
-`
+`;
 
 const Box = styled.div`
-  background-color: tomato;
-  height:200px;
-  width:200px;
+  background-color: ${(props) => props.bgC};
+  height: 200px;
+  width: 200px;
   display: flex;
   justify-content: center;
-  align-items:center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji}:hover{
-      font-size: 198px;
-    }
-`
+  align-items: center;
+  animation: ${rotationAnimation} 10s linear infinite;
+  ${Emoji}:hover {
+    font-size: 198px;
+  }
+  flex-direction: column;
+`;
 
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
 
 function App() {
   return (
     <>
-      <Father>
+      <Wrap>
         <Text as="a">as="a"</Text>
-        <Input/>
-        <Input/>
-        <Input/>
-        <Input/>
-        <Input/>
-        <Input/>
-        <Box>
-        <Emoji>🍎</Emoji>
+        <Input />
+        <Input />
+        <Input />
+        <Box bgC="teal">
+          <Emoji>🍎</Emoji>
+          <Circle bgC="red"></Circle>
         </Box>
-      </Father>
+      </Wrap>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+
+const darkMode = {
+  textColor:"white",
+  backgroundColor:"#111"
+};
+
+const lightMode = {
+  textColor:"#111",
+  backgroundColor:"white"
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <ThemeProvider theme={darkMode}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
